8dd2cf099cf3729f6ec9e0f22ff6743ef688c2a0
Jacopo Filié <Jackfelle@users.noreply.github.com>
2023-04-16 11:54:59 +0200

    ADD: added initial files of the project.

---
+++ app/src/main/java/com/jackfelle/jfkitexample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jackfelle.jfkitexample;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+{
+	@Override protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.activity_main);
+	}
+}
